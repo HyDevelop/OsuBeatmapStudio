bfc01fb71b5a3dae9db83736e6a14f148a2726d1
Hykilpikonna <admin@moecraft.cc>
2020-05-19 19:02:38 -0400

    [+] Create BeatmapColor class

---
+++ src/main/java/org/hydev/obp/BeatmapColor.kt
@@ -0,0 +1,22 @@
+package org.hydev.obp
+
+import java.awt.Color
+
+/**
+ * Beatmap combo color setting
+ * <p>
+ * Class created by the HyDEV Team on 2020-05-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-05-19 18:36
+ */
+class BeatmapColor(
+
+    // Additive combo colours
+    var combo: Int,
+
+    // New color
+    var color: Color
+)
