2cd05d6a80656a694d2520c8830f43dd2b7159ae
Hykilpikonna <admin@moecraft.cc>
2020-05-19 21:33:49 -0400

    [+] Create HitManiaHold class

---
+++ src/main/java/org/hydev/obp/objects/HitManiaHold.kt
@@ -0,0 +1,20 @@
+package org.hydev.obp.objects
+
+import org.hydev.obp.Beatmap
+
+/**
+ * Hold keys for mania
+ * x,y,time,type,hitSound,endTime,hitSample
+ * <p>
+ * Class created by the HyDEV Team on 2020-05-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-05-19 21:23
+ */
+class HitManiaHold(beatmap: Beatmap) : HitMania(beatmap)
+{
+    // End time of the hold, in milliseconds from the beginning of the beatmap's audio.
+    var endTime: Int = -1
+}
