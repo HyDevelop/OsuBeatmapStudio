750dd68f764239befbc7588b6b58d4d6a4d5569f
Hykilpikonna <admin@moecraft.cc>
2020-05-19 22:42:23 -0400

    [O] Optimize event data class

--- src/main/java/org/hydev/obp/BeatmapEvent.kt
+++ src/main/java/org/hydev/obp/BeatmapEvent.kt
@@ -13,23 +13,15 @@ import kotlinx.serialization.Serializable
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2020-05-19 17:47
  */
-@Serializable
-class BeatmapEvent(
-
+class BeatmapEvent
+{
     // Type of the event. Some events may be referred to by either a name or a number.
-    var eventType: String = "",
+    var eventType: String = ""
 
     // Start time of the event, in milliseconds from the beginning of the beatmap's audio.
     // For events that do not use a start time, the default is 0.
-    var startTime: Int = 0,
+    var startTime: Int = 0
 
     // (Comma-separated list): Extra parameters specific to the event's type.
     var eventParams: String = ""
-)
-{
-    /**
-     * Getter for EventParams
-     */
-    val eventParamsList: List<String>
-        get() = eventParams.split("");
 }
