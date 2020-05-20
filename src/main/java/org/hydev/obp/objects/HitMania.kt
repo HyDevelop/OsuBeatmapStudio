54505282d1e517aebf0385c2e66f53e8cbcd2599
Hykilpikonna <admin@moecraft.cc>
2020-05-19 21:32:18 -0400

    [+] Add column calculation for mania

--- src/main/java/org/hydev/obp/objects/HitMania.kt
+++ src/main/java/org/hydev/obp/objects/HitMania.kt
@@ -14,3 +14,9 @@ import java.util.*
  * @since 2020-05-19 20:50
  */
 open class HitMania(beatmap: Beatmap) : HitCircle(beatmap)
+{
+    // x for mania is the key index.
+    override var x: Int = super.x
+        get() = (beatmap.properties.circleSize / 512 / field).toInt()
+        set(value) { field = (value * beatmap.properties.circleSize / 512).toInt() }
+}
