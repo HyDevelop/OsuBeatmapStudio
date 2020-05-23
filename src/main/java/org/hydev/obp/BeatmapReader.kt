429a46546689cabbd46b39afdb829f99c8c23c5c
Hykilpikonna <admin@moecraft.cc>
2020-05-23 12:16:05 -0400

    [+] Create beatmap reader class

---
+++ src/main/java/org/hydev/obp/BeatmapReader.kt
@@ -0,0 +1,20 @@
+package org.hydev.obp
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.StringReader
+
+/**
+ * Beatmap parser that parses .osu files to Beatmap objects.
+ * <p>
+ * Class created by the HyDEV Team on 2020-05-23!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-05-23 11:49
+ */
+object BeatmapReader
+{
+
+}
