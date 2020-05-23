d1ed75be8581a36da987b01f97a7c58a61d69084
Hykilpikonna <admin@moecraft.cc>
2020-05-23 13:41:10 -0400

    [O] Enable default contrustor too

--- src/main/java/org/hydev/obp/BeatmapTimingPoint.kt
+++ src/main/java/org/hydev/obp/BeatmapTimingPoint.kt
@@ -13,35 +13,36 @@ import java.util.*
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2020-05-19 18:07
  */
-class BeatmapTimingPoint
-{
+class BeatmapTimingPoint(
+
     // Start time of the timing section, in milliseconds from the beginning of the beatmap's audio.
     // The end of the timing section is the next timing point's time (or never, if this is the last timing point).
-    var time: Int
+    var time: Int,
 
     // This property has two meanings: For uninherited timing points, the duration of a beat, in milliseconds.
     // For inherited timing points, a negative inverse slider velocity multiplier, as a percentage.
     // For example, -50 would make all sliders in this timing section twice as fast as SliderMultiplier.
-    var beatLength: Double
+    var beatLength: Double,
 
     // Amount of beats in a measure. Inherited timing points ignore this property.
-    var meter: Int
+    var meter: Int,
 
     // Default sample set for hit objects (0 = beatmap default, 1 = normal, 2 = soft, 3 = drum).
-    var sampleSet: Int
+    var sampleSet: Int,
 
     // Custom sample index for hit objects. 0 indicates osu!'s default hitsounds.
-    var sampleIndex: Int
+    var sampleIndex: Int,
 
     // Volume percentage for hit objects.
-    var volume: Int
+    var volume: Int,
 
     // Whether or not the timing point is uninherited.
-    var uninherited: Boolean
+    var uninherited: Boolean,
 
     // Bit flags that give the timing point extra effects. See [the effects section](#effects).
     var effects: BitSet
-
+)
+{
     /**
      * Parse from line
      */
