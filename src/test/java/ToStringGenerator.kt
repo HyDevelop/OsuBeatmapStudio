fb49c3712ad4f5256e4796986bd868a9b3ec9a19
Hykilpikonna <admin@moecraft.cc>
2020-05-23 15:19:38 -0400

    [+] Create toString generator

---
+++ src/test/java/ToStringGenerator.kt
@@ -0,0 +1,42 @@
+import org.hydev.obp.BeatmapProperties
+import java.lang.StringBuilder
+import java.util.*
+import kotlin.reflect.full.createType
+import kotlin.reflect.full.declaredMemberProperties
+
+/**
+ * TODO: Write a description for this class!
+ * <p>
+ * Class created by the HyDEV Team on 2020-05-23!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-05-23 15:09
+ */
+fun main(args: Array<String>)
+{
+    val target = BeatmapProperties::class
+    val result = StringBuilder("\"\"\"\n");
+
+    // All fields
+    for (field in target.declaredMemberProperties)
+    {
+        val name = field.name
+        val type = field.returnType
+        val line = "${name.capitalize()}:$" +
+            when (type)
+            {
+                Boolean::class.createType(), BitSet::class.createType() -> "{$name.num()}"
+                else -> name
+            }
+
+        // Assignment line
+        result.append(line).append("\n")
+    }
+
+    // Ending text block
+    result.append("\"\"\".trimIndent()")
+
+    println(result);
+}
