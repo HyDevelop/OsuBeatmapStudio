081e262450ae9cc02a35323f6540d4a5c1bcdf13
Hykilpikonna <admin@moecraft.cc>
2020-05-23 13:32:36 -0400

    [+] Create parser for timing points

--- src/main/java/org/hydev/obp/BeatmapTimingPoint.kt
+++ src/main/java/org/hydev/obp/BeatmapTimingPoint.kt
@@ -13,32 +13,49 @@ import java.util.*
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2020-05-19 18:07
  */
-class BeatmapTimingPoint(
-
+class BeatmapTimingPoint
+{
     // Start time of the timing section, in milliseconds from the beginning of the beatmap's audio.
     // The end of the timing section is the next timing point's time (or never, if this is the last timing point).
-    var time: Int,
+    var time: Int
 
     // This property has two meanings: For uninherited timing points, the duration of a beat, in milliseconds.
     // For inherited timing points, a negative inverse slider velocity multiplier, as a percentage.
     // For example, -50 would make all sliders in this timing section twice as fast as SliderMultiplier.
-    var beatLength: Double,
+    var beatLength: Double
 
     // Amount of beats in a measure. Inherited timing points ignore this property.
-    var meter: Int,
+    var meter: Int
 
     // Default sample set for hit objects (0 = beatmap default, 1 = normal, 2 = soft, 3 = drum).
-    var sampleSet: Int,
+    var sampleSet: Int
 
     // Custom sample index for hit objects. 0 indicates osu!'s default hitsounds.
-    var sampleIndex: Int,
+    var sampleIndex: Int
 
     // Volume percentage for hit objects.
-    var volume: Int,
+    var volume: Int
 
     // Whether or not the timing point is uninherited.
-    var uninherited: Boolean,
+    var uninherited: Boolean
 
     // Bit flags that give the timing point extra effects. See [the effects section](#effects).
     var effects: BitSet
-)
+
+    /**
+     * Parse from line
+     */
+    constructor(line: String)
+    {
+        // Line format: time,beatLength,meter,sampleSet,sampleIndex,volume,uninherited,effects
+        val split = line.split(",")
+        time = split[0].toInt()
+        beatLength = split[1].toDouble()
+        meter = split[2].toInt()
+        sampleSet = split[3].toInt()
+        sampleIndex = split[4].toInt()
+        volume = split[5].toInt()
+        uninherited = split[6].toBoolean()
+        effects = BitSet(split[7].toInt())
+    }
+}
