dc557c0fccce365b5401d23973f2193c7d071236
Hykilpikonna <admin@moecraft.cc>
2020-05-23 18:29:28 -0400

    [+] Create BitFlags class

---
+++ src/main/java/org/hydev/obp/BitFlags.kt
@@ -0,0 +1,15 @@
+package org.hydev.obp
+
+/**
+ * Bit flags
+ * <p>
+ * Class created by the HyDEV Team on 2020-05-23!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-05-23 18:03
+ */
+class BitFlags(var num: Int = 0)
+{
+}
