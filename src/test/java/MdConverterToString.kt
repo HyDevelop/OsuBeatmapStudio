55a1c4f339583b4a1c9fe33cbec2ab5fe02b7069
Hykilpikonna <admin@moecraft.cc>
2020-05-23 15:27:33 -0400

    [+] Create toString generator with md

---
+++ src/test/java/MdConverterToString.kt
@@ -0,0 +1,50 @@
+import java.util.*
+import kotlin.reflect.full.createType
+
+/**
+ * This script coverts the osu file format readme into kotlin variables.
+ * https://osu.ppy.sh/help/wiki/osu!_File_Formats/Osu_(file_format)
+ *
+ * Class created by the HyDEV Team on 2020-05-19!
+ *
+ * @author HyDEV Team (https://github.com/HyDevelop)
+ * @author Hykilpikonna (https://github.com/hykilpikonna)
+ * @author Vanilla (https://github.com/VergeDX)
+ * @since 2020-05-19 17:28
+ */
+fun main()
+{
+    val test = object {}.javaClass.getResource("/MdBeatmapProperties.txt").readText()
+            .replace("([`\r])".toRegex(), "")
+            .replace("(\\| | \\| | \\|)".toRegex(), "|").replace("||", "| |")
+            .replace("Integer", "Int").replace("Decimal", "Double").replace("0 or 1", "Boolean")
+
+    val output = StringBuilder("\"\"\"\n");
+
+    // Read by line
+    for (line in test.split("\n").toTypedArray())
+    {
+        // Empty line
+        if (line.isEmpty())
+        {
+            output.append("\n// Next Section\n")
+            continue
+        }
+
+        // Split
+        val split = line.split("|")
+        val name = split[1]
+        val type = split[2]
+
+        val line = "${name.capitalize()}:$" +
+            when (type)
+            {
+                "Boolean" -> "{$name.num()}"
+                else -> name
+            }
+
+        output.append(line).append("\n")
+    }
+
+    println("$output\"\"\".trimIndent()")
+}
