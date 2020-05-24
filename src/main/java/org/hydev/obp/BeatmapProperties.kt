84830a9d6f15d5b4afdba6fdad7393f928b095c5
Hykilpikonna <admin@moecraft.cc>
2020-05-24 11:30:14 -0400

    [O] Optimize default values of beatmap properties

--- src/main/java/org/hydev/obp/BeatmapProperties.kt
+++ src/main/java/org/hydev/obp/BeatmapProperties.kt
@@ -10,138 +10,138 @@ package org.hydev.obp
  * @author Vanilla (https://github.com/VergeDX)
  * @since 2020-05-19 16:28
  */
-class BeatmapProperties()
+class BeatmapProperties
 {
     // Location of the audio file relative to the current folder
-    var audioFilename: String = " ";
+    lateinit var audioFilename: String
 
     // Milliseconds of silence before the audio starts playing
-    var audioLeadIn: Int = 0;
+    var audioLeadIn: Int = 0
 
     // *Deprecated*
-    var audioHash: String = " ";
+    var audioHash: String? = null
 
     // Time in milliseconds when the audio preview should start
-    var previewTime: Int = -1;
+    var previewTime: Int = -1
 
     // Speed of the countdown before the first hit object
     // (0 = no countdown, 1 = normal, 2 = half, 3 = double)
-    var countdown: Int = 1;
+    var countdown: Int = 0
 
     // Sample set that will be used if timing points do not override it (Normal, Soft, Drum)
-    var sampleSet: String = "Normal";
+    var sampleSet: String = "Normal"
 
     // Multiplier for the threshold in time where hit objects placed close together stack (0–1)
-    var stackLeniency: Double = 0.7;
+    var stackLeniency: Double = 0.7
 
     // Game mode (0 = osu!, 1 = osu!taiko, 2 = osu!catch, 3 = osu!mania)
-    var mode: Int = 0;
+    var mode: Int = 0
 
     // Whether or not breaks have a letterboxing effect
-    var letterboxInBreaks: Boolean = false;
+    var letterboxInBreaks: Boolean = false
 
     // *Deprecated*
-    var storyFireInFront: Boolean = true;
+    var storyFireInFront: Boolean? = null // Default: true
 
     // Whether or not the storyboard can use the user's skin images
-    var useSkinSprites: Boolean = false;
+    var useSkinSprites: Boolean? = null // Default: false
 
     // *Deprecated*
-    var alwaysShowPlayfield: Boolean = false;
+    var alwaysShowPlayfield: Boolean? = null // Default: false
 
     // Draw order of hit circle overlays compared to hit numbers
     // (NoChange = use skin setting, Below = draw overlays under numbers, Above = draw overlays on top of numbers)
-    var overlayPosition: String = "NoChange";
+    var overlayPosition: String? = null // Default: "NoChange"
 
     // Preferred skin to use during gameplay
-    var skinPreference: String = " ";
+    var skinPreference: String? = null // Default: " "
 
     // Whether or not a warning about flashing colours should be shown at the beginning of the map
-    var epilepsyWarning: Boolean = false;
+    var epilepsyWarning: Boolean? = null // Default: false
 
     // Time in beats that the countdown starts before the first hit object
-    var countdownOffset: Int = 0;
+    var countdownOffset: Int? = null // Default: 0
 
     // Whether or not the "N+1" style key layout is used for osu!mania
-    var specialStyle: Boolean = false;
+    var specialStyle: Boolean? = null // Default: false
 
     // Whether or not the storyboard allows widescreen viewing
-    var widescreenStoryboard: Boolean = false;
+    var widescreenStoryboard: Boolean = false
 
     // Whether or not sound samples will change rate when playing with speed-changing mods
-    var samplesMatchPlaybackRate: Boolean = false;
+    var samplesMatchPlaybackRate: Boolean? = null // Default: false
 
 
     // Editor
 
     // Time in milliseconds of bookmarks
-    var bookmarks: ArrayList<Int> = ArrayList();
+    var bookmarks: ArrayList<Int>? = null
 
     // Distance snap multiplier
-    var distanceSpacing: Double? = null;
+    var distanceSpacing: Double? = null
 
     // Beat snap divisor
-    var beatDivisor: Double? = null;
+    var beatDivisor: Double? = null
 
     // Grid size
-    var gridSize: Int? = null;
+    var gridSize: Int? = null
 
     // Scale factor for the object timeline
-    var timelineZoom: Double? = null;
+    var timelineZoom: Double? = null
 
 
     // Metadata
 
     // Romanised song title
-    var title: String = "";
+    var title: String = ""
 
     // Song title
-    var titleUnicode: String = "";
+    var titleUnicode: String = ""
 
     // Romanised song artist
-    var artist: String = "";
+    var artist: String = ""
 
     // Song artist
-    var artistUnicode: String = "";
+    var artistUnicode: String = ""
 
     // Beatmap creator
-    var creator: String = "";
+    var creator: String = ""
 
     // Difficulty name
-    var version: String = "";
+    var version: String = ""
 
     // Original media the song was produced for
-    var source: String = "";
+    var source: String = ""
 
     // Search terms
-    var tags: ArrayList<String> = ArrayList();
+    var tags: ArrayList<String> = ArrayList()
 
     // Beatmap ID
-    var beatmapID: Int = -1;
+    var beatmapID: Int = 0
 
     // Beatmapset ID
-    var beatmapSetID: Int = -1;
+    var beatmapSetID: Int = -1
 
 
     // Difficulty
 
     // HP setting (0–10)
-    var hpDrainRate: Double = -1.0;
+    var hpDrainRate: Double = 5.0
 
     // CS setting (0–10)
-    var circleSize: Double = -1.0;
+    var circleSize: Double = 5.0
 
     // OD setting (0–10)
-    var overallDifficulty: Double = -1.0;
+    var overallDifficulty: Double = 5.0
 
     // AR setting (0–10)
-    var approachRate: Double = -1.0;
+    var approachRate: Double = 5.0
 
     // Base slider velocity in hecto-[osu! pixels](/wiki/Glossary#osupixel) per beat
-    var sliderMultiplier: Double = -1.0;
+    var sliderMultiplier: Double = 1.4
 
     // Amount of slider ticks per beat
-    var sliderTickRate: Double = -1.0;
+    var sliderTickRate: Double = 1.0
 
 
     /**
